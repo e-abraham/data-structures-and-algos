2a80e45948c8cfd88dcf5713f5837b5fdaef17aa
abrahel <elizabeth.abraham@verizon.com>
2022-03-10 11:41:12 -0600

    Add countdown

--- algos/recursion/countdown/index.js
+++ algos/recursion/countdown/index.js
@@ -1,4 +1,12 @@
 function countdown(n){
+    // String counts down from n, replace 0 with Liftoff!
+    // Join each string to the next separated by comma space
+    // neg n goes straight to Liftoff!
+    if (n <= 0) {
+        return "Liftoff!"; // O(1)
+    } else {
+        return `${n}, ` + countdown(n-1); // O(n)
+    } // O(n) solution
 
 }
 
