c1736d7378e3fe8f88e7ecca422e5ad9bda109eb
abrahel <elizabeth.abraham@verizon.com>
2022-03-10 12:09:50 -0600

    refactor isPalendrome

--- algos/is-palendrome/index.js
+++ algos/is-palendrome/index.js
@@ -2,11 +2,19 @@ function isPalendrome(str){
 	// Complete the function!
 	let revStr = str.split('').reverse().join('');
 	console.log(`string: ${str} \n reverse: ${revStr}`);
-	if (str === revStr) {
-		return true;
-	} else {
-		return false;
+	return str === revStr;
+	
+
+	/* // Alternative solution
+	let first, last;
+	while (str.length > 1){
+		first = str[0];
+		last = str[str.length - 1];
+		if (first != last) return false;
+		str = str.slice(1, str.length - 1);
 	}
+	return true;
+	*/
 }
 
 
