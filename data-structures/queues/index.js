c278957581e464696c75e9e9c32181203578ee69
abrahel <elizabeth.abraham@verizon.com>
2022-03-15 16:20:31 -0500

    Add queue

--- data-structures/queues/index.js
+++ data-structures/queues/index.js
@@ -1,7 +1,19 @@
 class Queue {
 	// Your code below!
 	constructor() {
+		this.elements = [];
+		this.size = 0;
+	}
 
+	enqueue(element){
+		this.elements.push(element);
+		this.size = this.elements.length;
+	}
+
+	dequeue(){
+		let first = this.elements.shift();
+		this.size = this.elements.length;
+		return first;
 	}
 }
 
