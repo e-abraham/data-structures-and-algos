3017fe0a37e95c0ed286c82adca82cfe460812ff
abrahel <elizabeth.abraham@verizon.com>
2022-03-16 17:00:51 -0500

    Add stacks queues alternative

--- data-structures/queues/index.js
+++ data-structures/queues/index.js
@@ -8,12 +8,24 @@ class Queue {
 	enqueue(element){
 		this.elements.push(element);
 		this.size = this.elements.length;
+		/* // Alternative immutable solution
+		// set to new array add at end, use spreader to include previous
+		this.elements = [...this.elements, element];
+		this.size = this.elements.length;
+		*/
 	}
 
 	dequeue(){
 		let first = this.elements.shift();
 		this.size = this.elements.length;
 		return first;
+		/* // Alternative immutable solution
+		// set to new array of previous skipping first
+		let first = this.elements[0];
+		this.elements = this.elements.slice(1);
+		this.size = this.elements.length;
+		return first;
+		*/
 	}
 }
 
--- data-structures/stacks/index.js
+++ data-structures/stacks/index.js
@@ -8,12 +8,24 @@ class Stack {
 	push(element){
 		this.elements.push(element);
 		this.size = this.elements.length;
+		/* // Alternative immutable solution
+		// set to new array add first, use spreader to include previous
+		this.elements = [element, ...this.elements];
+		this.size = this.elements.length;
+		*/
 	}
 
 	pop(){
 		let last = this.elements.pop();
 		this.size = this.elements.length;
 		return last;
+		/* // Alternative immutable solution
+		// set to new array of previous skipping first
+		let first = this.elements[0];
+		this.elements = this.elements.slice(1);
+		this.size = this.elements.length;
+		return first;
+		*/
 	}
 }
 
