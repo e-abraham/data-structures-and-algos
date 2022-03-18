528e3ee7af2cdb914fa2adb73f1e230a39fc0f66
abrahel <elizabeth.abraham@verizon.com>
2022-03-18 17:52:09 -0500

    Fix how binary search tree handles duplicates

--- data-structures/bst/index.js
+++ data-structures/bst/index.js
@@ -2,18 +2,18 @@ class BinarySearchTree {
   // Your code here!
   constructor (root) {
     this.value = root;
-    this.left = null;
-    this.right = null;
+    this.left = null; // Less than root value
+    this.right = null; // Greater than or duplicate root value
   }
 
   insert(newValue){
-    if (newValue < this.value){
+    if (newValue < this.value){ // Left side insert, less than
       if (!this.left){ // Base Case
         this.left = new BinarySearchTree(newValue);
       } else { // Recursive Case
         this.left.insert(newValue);
       }
-    } else if (newValue > this.value){
+    } else { // Right side insert, greater than or duplicate
       if (!this.right){ // Base Case
         this.right = new BinarySearchTree(newValue);
       } else { // Recursive Case
@@ -21,31 +21,28 @@ class BinarySearchTree {
       }
     }
     return newValue;
-    /* // Another In Progress
-    if (this.value == newValue){
-      return this.value;
-    } else {
-      let child = newValue < this.value ? this.left : this.right;
-      if (!child) {
-        child = new BinarySearchTree(newValue);
-      } else {
-        child.insert(newValue);
-      }
+    /* // Another Solution
+    if (newValue < this.value){ // Left side insert, less than
+      if (!this.left) return this.left = new BinarySearchTree(newValue); // Base Case
+      this.left.insert(newValue); // Recursive case
+    } else { // Right side insert, greater than or duplicate
+      if (!this.right) return this.right = new BinarySearchTree(newValue); // Base Case
+      this.right.insert(newValue); // Recursive case
     }
+    return newValue;
     */
   }
 
   contains(value){
-    if (this.value === value){ // Base Case
+    if (this.value == value){ // Base Case
       return true
     } else { // Recursive Case
-      let child = value < this.value ? this.left : this.right;
+      let child = value < this.value ? this.left : this.right; // Side to check
       return child ? child.contains(value) : false;
       /* // Another Solution
-      if (value < this.value){
+      if (value < this.value){ // Left side check, less than
         return this.left ? this.left.contains(value) : false;
-      }
-      if (value > this.value){
+      } else { // Right side check, greater than or duplicates
         return this.right ? this.right.contains(value) : false;
       }
       */
